e37ac52e37b78af618afe4502838d7b174592426
edolilloYahoo <103328454+edolilloYahoo@users.noreply.github.com>
2022-04-18 22:53:11 -0400

    Create parametrizadoDSL.groovy

---
+++ parametrizadoDSL.groovy
@@ -0,0 +1,40 @@
+job('ejeplo-job-DSL') {
+			description('DSLde ejemplo para el curso de Jenkins')
+		  scm {
+				git('https://github.com/edolilloYahoo/-jenkins.job.parametrizado.git', 'main') { node -> // is hudson.plugins.git.GitSCM
+					node / gitConfigName('edolillo')
+					node / gitConfigEmail('edolillo@ymail.com')
+				}
+			}
+		  parameters {
+				stringParam('nombre', defaultValue = 'Eduardo', description = 'Parametros de cadena para el Job DSL')
+				choiceParam('Planeta', ['Mercurio (default)', 'Venus', 'tierra', 'Marte', 'Jupiter','Sarturo', 'Urano', 'Pluton'])
+				booleanParam('Agente', false)
+			}
+		  triggers {
+				cron('H/7 * * * *')
+			}
+		  steps {
+				shell("bash jobscript.sh")
+		   }
+		  publishers {
+				mailer('edolillo@ymail.com', true, true)
+				slackNotifier {
+				  notifyAborted(true)
+				  notifyEveryFailure(true)
+				  notifyNotBuilt(false)
+				  notifyUnstable(false)
+				  notifyBackToNormal(true)
+				  notifySuccess(false)
+				  notifyRepeatedFailure(false)
+				  startNotification(false)
+				  includeTestSummary(false)
+				  includeCustomMessage(false)
+				  customMessage(null)
+				  sendAs(null)
+				  commitInfoChoice('NONE')
+				  teamDomain(null)
+				  authToken(null)
+				}
+		  }
+		}
